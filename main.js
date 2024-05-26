50bf58a11fda089416a6a32af5e0b5592d91eda9
Chaan <rnchagwe27@gmail.com>
2024-05-26 17:28:55 +0800

    js done

--- main.js
+++ main.js
@@ -2,9 +2,17 @@ let seconds = 0
 let interval = null
 const time = document.getElementById("time")
 const start = document.getElementById("start")
+const stoP = document.getElementById("stop")
+const reset = document.getElementById("reset")
+
+function padStart(value) {
+    return String(value).padStart(2, "0")
+}
 
 function setTime() {
-    time.innerText = seconds;
+    const minutes = Math.floor(seconds / 60)
+    const secs = seconds % 60
+    time.innerText = `${padStart(minutes)}:${padStart(seconds)}`;
 }
 
 function timer() {
@@ -13,17 +21,28 @@ function timer() {
 }
 
 function startClock() {
+    if (interval) stopClock()
     interval = setInterval(timer, 1000)
 }
 
 function stopClock() {
-
+    clearInterval(interval)
 }
 
 function resetClock() {
-
+    stopClock()
+    seconds = 0
+    setTime()
 }
 
 start.addEventListener("click", function() {
     startClock()
+})
+
+stoP.addEventListener("click", function() {
+    stopClock()
+})
+
+reset.addEventListener("click", function() {
+    resetClock()
 })
